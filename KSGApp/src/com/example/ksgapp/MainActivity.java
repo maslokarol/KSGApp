721098fb4ef04eee02a883858759eccf8b4f09c6
maslokarol <maslowski.karol@gmail.com>
2014-12-12 01:06:13 +0100

    Adding local path to project.properties

--- KSGApp/src/com/example/ksgapp/MainActivity.java
+++ KSGApp/src/com/example/ksgapp/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.ksgapp.R;
 
 import com.example.dialogs.TitleDialog;
 import com.google.android.gms.maps.CameraUpdateFactory;
